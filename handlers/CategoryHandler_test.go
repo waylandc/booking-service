11a83a0d6ada2c3daeeb548418cf0b7cf4ad08fd
wchan <wayland.chan@gmail.com>
2018-03-19 02:45:51 +0800

    fix test case

--- handlers/CategoryHandler_test.go
+++ handlers/CategoryHandler_test.go
@@ -20,6 +20,18 @@ func (dao CategoryDAOStub) Create(c *models.Category) error {
 	return nil
 }
 
+func (dao CategoryDAOStub) Delete(c *models.Category) error {
+	return nil
+}
+
+func (dao CategoryDAOStub) Update(c *models.Category) error {
+	return nil
+}
+
+func (dao CategoryDAOStub) Find(name string) ([]models.Category, error) {
+	return make([]models.Category, 0), nil
+}
+
 func TestCreate(t *testing.T) {
 	values := map[string]string{"name": "Restaurants"}
 	jsonValue, _ := json.Marshal(values)
@@ -36,6 +48,24 @@ func TestCreate(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	assert.Equal(t, rr.Code, http.StatusOK)
+}
+
+func TestCreateBadRequest(t *testing.T) {
+	values := map[string]string{"wrongfield": "should fail"}
+	jsonValue, _ := json.Marshal(values)
+
+	req, err := http.NewRequest("POST", "/category", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	service := &services.CategoryService{DAO: &CategoryDAOStub{}}
+	handler := CategoryHandler{service}
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusBadRequest)
 }
 
 func TestCreateWithEmptyField(t *testing.T) {
--- interfaces/ICategoryService.go
+++ interfaces/ICategoryService.go
@@ -5,7 +5,7 @@ import "wchan/booking-service/models"
 // ICategoryService interface
 type ICategoryService interface {
 	Create(c models.Category) error
-	Delete(c *models.Category) error
-	Update(c *models.Category) error
+	Delete(c models.Category) error
+	Update(c models.Category) error
 	Find(name string) ([]models.Category, error)
 }
